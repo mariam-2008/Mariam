16df6f8f41a9b25030e3be9942d1b32821327b9e
mariam-2008 <cit-ai-25111@uofallujah.edu.iq>
2026-05-04 01:30:27 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    cout << "Multiplication Table for " << number << ":\n";
+
+    for (int i = 1; i <= 10; i++) {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+
+    return 0;
+}
